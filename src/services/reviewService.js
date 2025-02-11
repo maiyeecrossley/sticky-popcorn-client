90c2c98b997dec1cb6817793b852db930c8cd096
maiyeecrossley <maiyee.crossley@gmail.com>
2025-02-11 15:52:46 +0000

    added SingleReview component

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -7,8 +7,19 @@ export const reviewIndex = async (movieId) => {
         const response = await axios.get(`${BASE_URL}/movies/${movieId}/reviews`)
         return response.data
     } catch (error) {
-        console.log("Error fetching reviews", error)
+        console.log(error)
         throw error
+    }
+}
+
+export const reviewShow = async (movieId, reviewId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/movies/${movieId}/reviews/${reviewId}`)
+        return response.data
+    } catch (error) {
+        console.log(error)
+        throw error
+
     }
 }
 
